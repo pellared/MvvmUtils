b934ae149c7395666dfc0355a7e6332860dda023
Robert Pająk <pellared@hotmail.com>
2014-10-03 12:37:04 +0200

    Small enchancement

--- SalaryBook/SalaryBook/Common/Validation/CompositeValidator.cs
+++ SalaryBook/SalaryBook/Common/Validation/CompositeValidator.cs
@@ -4,7 +4,7 @@ namespace Pellared.Utils.Mvvm.Validation
 {
     public class CompositeValidator<TObject> : CompositeValidator<TObject, ValidationError>, IValidator<TObject>
     {
-        public CompositeValidator(params IValidator<TObject, ValidationError>[] validators)
+        public CompositeValidator(params IValidator<TObject>[] validators)
             : base(validators)
         {
             
